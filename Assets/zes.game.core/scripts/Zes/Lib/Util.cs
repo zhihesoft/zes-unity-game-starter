916a27cd370abe438d562df95f817502182eac0d
stanley <stanley@zhihesoft.com>
2022-08-17 18:39:09 +0800

    loader phase 2

---
+++ Assets/zes.game.core/scripts/Zes/Lib/Util.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Zes
+{
+    public static class Util
+    {
+        public static async Task WaitAsyncOperation(AsyncOperation op, Action<float> progress = null)
+        {
+            while (!op.isDone)
+            {
+                progress?.Invoke(op.progress);
+                await Task.Delay(0);
+            }
+            progress?.Invoke(1);
+        }
+    }
+}
