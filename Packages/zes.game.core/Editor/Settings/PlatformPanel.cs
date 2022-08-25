b290cf3ff466e06c2c64b44725fef3c64e078e37
stanley <stanley@zhihesoft.com>
2022-08-25 18:43:20 +0800

    update config editor

---
+++ Packages/zes.game.core/Editor/Settings/PlatformPanel.cs
@@ -0,0 +1,17 @@
+﻿namespace Zes.Settings
+{
+    public class PlatformPanel : SettingPanel
+    {
+        public override string Name => "Platform";
+
+        public override string DisplayName => "Platform";
+
+        public override string Description => "Platform settings";
+
+        public override void OnGUI()
+        {
+            platformConfig.androidKeystorePassword = TextField("Android keystore pwd", platformConfig.androidKeystorePassword);
+            platformConfig.androidKeyAliasPassword = TextField("Android keyalias pwd", platformConfig.androidKeyAliasPassword);
+        }
+    }
+}
