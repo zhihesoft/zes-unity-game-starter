370cd76b79c01534998106ba41509c0122714b91
stanley <stanley@zhihesoft.com>
2022-09-01 21:12:02 +0800

    dummy of page_controls

--- Typescripts/src/lib/page_controls.ts
+++ Typescripts/src/lib/page_controls.ts
@@ -1,4 +1,7 @@
-import { click, component, page, PageService, Transit } from "zes-unity-jslib";
+import { UnityEngine } from "csharp";
+import { $typeof } from "puerts";
+import { BehaviorSubject } from "rxjs";
+import { bind, click, component, page, PageService, Transit, waitForSeconds } from "zes-unity-jslib";
 
 @component({ template: "Assets/Bundles/ui/controls.prefab" })
 @page({ transit: Transit.Fade })
@@ -6,6 +9,18 @@ export class PageControls {
     constructor(
         public pages: PageService,
     ) { }
+
+    @bind("#slider", { type: $typeof(UnityEngine.UI.Slider), prop: "value" })
+    slider: BehaviorSubject<number> = new BehaviorSubject(0);
+
+    @click("#btn-reset-slider", 10)
+    async onClickReset() {
+        this.slider.next(0);
+        while (this.slider.value < 1) {
+            this.slider.next(this.slider.value + 0.01);
+            await waitForSeconds(1 / 60);
+        }
+    }
 
     @click("#btn-return")
     onClickReturn() {
